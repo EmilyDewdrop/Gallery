62bc914eafbdfefca1f8833580558874924c46c9
EmilyDewdrop <turner.ek@gmail.com>
2023-03-04 22:14:24 +0000

    JS completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function toggleText() {
+function toggleText1() {
     var text = document.getElementById("hobbyText");
     if (text.style.display === "none") {
       text.style.display = "block";
@@ -7,3 +7,29 @@ function toggleText() {
     }
   }
 
+  function toggleText2() {
+    var text = document.getElementById("movieText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function toggleText3() {
+    var text = document.getElementById("websiteText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function toggleText4() {
+    var text = document.getElementById("gameText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }

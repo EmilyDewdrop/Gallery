6f725da72513f0d10cb83850f1c57d32d8bf0e5b
EmilyDewdrop <turner.ek@gmail.com>
2023-03-04 21:18:41 +0000

    Working hobby button

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleText() {
+    var text = document.getElementById("hobbyText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
